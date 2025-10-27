feb4aab9edbd741ab5418cc571153997a00fa8b6
lorrettalu <lu10@ucsb.edu>
2025-10-27 00:26:08 -0700

    finished pibble game

--- game1/pibble.js
+++ game1/pibble.js
@@ -37,8 +37,8 @@ function setup() {
 function draw() {
     background(230, 255, 230);
     image(pibble, width / 2, height / 2, 250, 250);
-    image(sponge, 440, 50, 100, 100);
-    image(rag, 440, 140, 100, 100);
+    image(rag, ragPos.x, ragPos.y, 100, 100);
+    image(sponge, spongePos.x, spongePos.y, 100, 100);
 
     // Main Game States
     if (gameState == "wash") {
@@ -48,14 +48,20 @@ function draw() {
     }
 
     // Text Feedback
-    if (gameState == "yay") {
+    if (gameState == "wash") {
         push();
         fill(70);
         textSize(22);
+        text("wash mah belly!", width / 2, height - 25);
+    }
+    if (gameState == "yay") {
         text("yaaayyyyyyy", width / 2, height - 25);
     }
+    if (gameState == "clean") {
+        text("clean mah belly!", width / 2, height - 25);
+    }
     if (gameState == "done") {
-        text("All done!", width / 2, height - 25);
+        text("i'm very happy!", width / 2, height - 25);
         pop();
     }
 
@@ -66,6 +72,7 @@ function draw() {
         rect(width / 2 - 100, 20, 200, 15, 10);
         fill(120, 200, 120);
         rect(width / 2 - 100, 20, map(progress, 0, 100, 0, 200), 15, 10);
+        pop();
     }
 
 }
@@ -79,23 +86,57 @@ function handleScrubbing(tool) {
     }
 
     if (progress >= 100 && gameState === "wash") {
+        washSound.stop();
         progress = 0;
         gameState = "yay";
         yaySound.play();
         setTimeout(() => {
-            gameState == "clean";
-            cleanSound.play();
+            gameState = "clean";
+            cleanSound.loop();
         }, 2000);
     } else if (progress >= 100 && gameState === "clean") {
         progress= 0;
-        gameState == "done";
+        gameState = "done";
         yaySound.play();
+        cleanSound.stop();
+
+        document.getElementById("nextBtn").style.display = "inline-block";
     }
 
 }
 
 function mousePressed() {
-    if (gameState === "wash" && dist(mouseX, mouseY, spongePos.x, spongePos.y) < 50) {
+    if (dist(mouseX, mouseY, spongePos.x, spongePos.y) < 50) {
         draggingSponge = true;
+    }
+    if (gameState === "wash" && dist(mouseX, mouseY, spongePos.x, spongePos.y) < 50) {
+        washSound.play();
+    }
+    if (dist(mouseX, mouseY, ragPos.x, ragPos.y) < 50) {
+        draggingRag = true;
+    }
+}
+
+function mouseDragged() {
+    if (draggingSponge) {
+        spongePos.x = mouseX;
+        spongePos.y = mouseY;
+    }
+    if (draggingRag) {
+        ragPos.x = mouseX;
+        ragPos.y = mouseY;
+    }
+}
+
+function mouseReleased() {
+    draggingSponge = false;
+    draggingRag = false;
+
+    if (gameState === "wash") {
+        spongePos.set(440, 50);
+    }
+
+    if (gameState === "clean") {
+        ragPos.set(440, 140, 100, 100);
     }
 }
