5a01865bc8d9b25f192155cae5746139ecb58acc
lorrettalu <lu10@ucsb.edu>
2025-10-28 00:13:38 -0700

    Added game 2 UI and asset variables

---
+++ game2/picture.js
@@ -0,0 +1,39 @@
+// Visual Assets
+let pic1;
+let pic2;
+let pic3;
+let pic4;
+let pic5;
+let pic6;
+let pic7;
+let pic8;
+let pic9;
+let pic10;
+let pic11;
+let pic12;
+let pic13;
+let pic14;
+let pic15;
+let pic16;
+let pic17;
+let pic18;
+let pic19;
+let pic20;
+let pic21;
+let pic22;
+let pic23;
+let pic24;
+let pic25;
+let pic26;
+let pic27;
+let pic28;
+let pic29;
+let pic30;
+
+// Sound Assets
+let el_dorado;
+let pink_skies;
+let ticking;
+let tournaquet;
+
+// Game Variables
