433391063efc13bb07bf3507713e2b3fa8ef9480
lorrettalu <lu10@ucsb.edu>
2025-11-03 23:47:26 -0800

    Added pictures, answers, and loaded audio

--- game2/picture.js
+++ game2/picture.js
@@ -1,39 +1,99 @@
 // Visual Assets
-let pic1;
-let pic2;
-let pic3;
-let pic4;
-let pic5;
-let pic6;
-let pic7;
-let pic8;
-let pic9;
-let pic10;
-let pic11;
-let pic12;
-let pic13;
-let pic14;
-let pic15;
-let pic16;
-let pic17;
-let pic18;
-let pic19;
-let pic20;
-let pic21;
-let pic22;
-let pic23;
-let pic24;
-let pic25;
-let pic26;
-let pic27;
-let pic28;
-let pic29;
-let pic30;
+const images = [
+    "joseph pictures/20220615_165224.JPG",
+    "joseph pictures/Image-1.JPG",
+    "joseph pictures/IMG_1212.JPG",
+    "joseph pictures/IMG_1362.JPG",
+    "joseph pictures/IMG_1961.jpg",
+    "joseph pictures/IMG_2465.JPG",
+    "joseph pictures/IMG_4478.JPG",
+    "joseph pictures/IMG_5124.JPG",
+    "joseph pictures/IMG_5783.JPG",
+    "joseph pictures/IMG_5835.JPG",
+    "joseph pictures/IMG_5996.JPG",
+    "joseph pictures/IMG_6422.JPG",
+    "joseph pictures/IMG_6426.JPG",
+    "joseph pictures/IMG_6752.JPG",
+    "joseph pictures/IMG_6766.JPG",
+    "joseph pictures/IMG_6767.JPG",
+    "joseph pictures/IMG_6851.JPG",
+    "joseph pictures/IMG_6943.JPG",
+    "joseph pictures/IMG_7279.JPG",
+    "joseph pictures/IMG_7390 2.JPG",
+    "joseph pictures/IMG_7558.JPG",
+    "joseph pictures/IMG_7661.JPG",
+    "joseph pictures/IMG_7766.JPG",
+    "joseph pictures/IMG_7798.JPG",
+    "joseph pictures/IMG_8039.JPG",
+    "joseph pictures/IMG_8137.JPG",
+    "joseph pictures/IMG_8458.JPG",
+    "joseph pictures/IMG_8674.JPG",
+    "joseph pictures/IMG_8675.JPG"
+];
+
+// Picture Answers
+const answers = [
+    ["norco", "pumpkin rock"],
+    ["irvine", "hello kitty", "hello kitty cafe", "cafe"],
+    ["macy's", "victoria gardens", "rancho cucamonga"],
+    ["park", "providence ranch", "providence ranch park", "eastvale"],
+    ["bathroom", "mcdonalds", "mcdonald's"],
+    ["victoria gardens", "h&m", "rancho cucamonga"],
+    ["santa anita", "h&m"],
+    ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
+    ["manzanita", "dorm", "manzi"],
+    ["manzanita", "dorm", "manzi"],
+    ["sb", "beach", "santa barbara", "ucsb"],
+    ["sb", "santa barbara", "manzanita", "manzi", "ucsb"],
+    ["girvetz", "girvetz hall"],
+    ["chica state beach", "chica", "chica state"],
+    ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
+    ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
+    ["santa ynez"],
+    ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
+    ["goleta butterfly grove", "butterfly", "butterfly grove"],
+    ["ucsb", "north hall"],
+    ["ucsb", "campbell hall"],
+    ["santa ynez"],
+    ["buena park", "medieval times"],
+    ["buena park", "medieval times"],
+    ["student health"],
+    ["lizard's mouth", "lizards mouth"],
+    ["sb", "santa barbara", "beach", "ucsb"],
+    ["sb", "santa barbara", "beach", "ucsb"],
+    ["sb", "santa barbara", "beach", "ucsb"] 
+];
 
 // Sound Assets
 let el_dorado;
 let pink_skies;
 let ticking;
-let tournaquet;
+let tourniquet;
 
 // Game Variables
+const g2_total = images.length;
+let img;
+let idx = 0;
+let reveal = false;
+let canvasW = 640;
+let canvasH = 400;
+let flashRadius = 90;
+
+function preload() {
+    el_dorado = loadSound("music/Zach Bryan - El Dorado.mp3");
+    pink_skies = loadSound("music/Zach Bryan - Pink Skies.mp3");
+    ticking = loadSound("music/Zach Bryan - Ticking.mp3");
+    tourniquet = loadSound("music/Zach Bryan - Tourniquet.mp3");
+}
+
+function setup() {
+    const cnv = createCanvas(canvasW, canvasH);
+    cnv.parent("game2-canvas");
+    imageMode(CENTER);
+    textAlign(CENTER, CENTER);
+    textSize(16);
+
+    const form = document.getElementById("g2-form");
+    const input = document.getElementById("g2-input");
+    const nextBtn = document.getElementById("g2-next");
+}
