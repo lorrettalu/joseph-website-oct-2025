69a926b307592c96b682f2d0ffa56e3688a49669
lorrettalu <lu10@ucsb.edu>
2025-11-23 02:53:15 -0800

    Fixed global instance bug

--- game1/pibble.js
+++ game1/pibble.js
@@ -1,144 +1,149 @@
-// Visual Assets
-let pibble;
-let sponge;
-let rag;
+let pibbleSketch = function(p) {
 
-// Sound Assets
-let washSound;
-let yaySound;
-let cleanSound;
+    // Visual Assets
+    let pibble;
+    let sponge;
+    let rag;
 
-// Game Variables
-let gameState = "wash";
-let progress = 0;
-let spongePos;
-let ragPos;
-let draggingSponge = false;
-let draggingRag = false;
-let canvasW = 500;
-let canvasH = 500;
+    // Sound Assets
+    let washSound;
+    let yaySound;
+    let cleanSound;
 
-function preload() {
-    pibble = loadImage("pibble pictures/pibble.png");
-    sponge = loadImage("pibble pictures/sponge.png");
-    rag = loadImage("pibble pictures/rag.png");
-    washSound = loadSound("soundfx/pibble_wash.mp3");
-    yaySound = loadSound("soundfx/pibble_yay.mp3");
-    cleanSound = loadSound("soundfx/pibble_clean.mp3");
+    // Game Variables
+    let gameState = "wash";
+    let progress = 0;
+    let spongePos;
+    let ragPos;
+    let draggingSponge = false;
+    let draggingRag = false;
+    let canvasW = 500;
+    let canvasH = 500;
+
+    p.preload = function() {
+        pibble = p.loadImage("pibble pictures/pibble.png");
+        sponge = p.loadImage("pibble pictures/sponge.png");
+        rag = p.loadImage("pibble pictures/rag.png");
+        washSound = p.loadSound("soundfx/pibble_wash.mp3");
+        yaySound = p.loadSound("soundfx/pibble_yay.mp3");
+        cleanSound = p.loadSound("soundfx/pibble_clean.mp3");
+    };
+
+    p.setup = function() {
+        let canvas = p.createCanvas(canvasW, canvasH);
+        canvas.parent("game1-container");
+        p.imageMode(p.CENTER);
+        p.textAlign(p.CENTER, p.CENTER);
+        spongePos = p.createVector(440, 50);
+        ragPos = p.createVector(440, 140);
+    };
+
+    p.draw = function() {
+        p.background(230, 255, 230);
+        p.image(pibble, p.width / 2, p.height / 2, 250, 250);
+        p.image(rag, ragPos.x, ragPos.y, 100, 100);
+        p.image(sponge, spongePos.x, spongePos.y, 100, 100);
+
+        // Main Game States
+        if (gameState == "wash") {
+            handleScrubbing("sponge");
+        } else if (gameState == "clean") {
+            handleScrubbing("rag");
+        }
+
+        // Text Feedback
+        if (gameState == "wash") {
+            p.push();
+            p.fill(70);
+            p.textSize(22);
+            p.text("wash mah belly!", p.width / 2, p.height - 25);
+        }
+        if (gameState == "yay") {
+            p.text("yaaayyyyyyy", p.width / 2, p.height - 25);
+        }
+        if (gameState == "clean") {
+            p.text("clean mah belly!", p.width / 2, p.height - 25);
+        }
+        if (gameState == "done") {
+            p.text("i'm very happy!", p.width / 2, p.height - 25);
+            p.pop();
+        }
+
+        // Progress Bar
+        if (gameState == "wash" || gameState == "clean") {
+            p.push();
+            p.fill(255);
+            p.rect(p.width / 2 - 100, 20, 200, 15, 10);
+            p.fill(120, 200, 120);
+            p.rect(p.width / 2 - 100, 20, p.map(progress, 0, 100, 0, 200), 15, 10);
+            p.pop();
+        }
+
+    };
+
+    function handleScrubbing(tool) {
+        let toolPos = (tool == "sponge") ? spongePos : ragPos;
+        let dragging = (tool == "sponge") ? draggingSponge : draggingRag;
+
+        if (dragging && p.dist(toolPos.x, toolPos.y, p.width / 2, p.height / 2 - 20) < 120) {
+            progress += 0.8;
+        }
+
+        if (progress >= 100 && gameState === "wash") {
+            washSound.stop();
+            progress = 0;
+            gameState = "yay";
+            yaySound.play();
+            setTimeout(() => {
+                gameState = "clean";
+                cleanSound.loop();
+            }, 2000);
+        } else if (progress >= 100 && gameState === "clean") {
+            progress= 0;
+            gameState = "done";
+            yaySound.play();
+            cleanSound.stop();
+
+            document.getElementById("nextGameBtn").style.display = "inline-block";
+        }
+
+    }
+
+    p.mousePressed = function() {
+        if (p.dist(p.mouseX, p.mouseY, spongePos.x, spongePos.y) < 50) {
+            draggingSponge = true;
+        }
+        if (gameState === "wash" && p.dist(p.mouseX, p.mouseY, spongePos.x, spongePos.y) < 50) {
+            washSound.play();
+        }
+        if (p.dist(p.mouseX, p.mouseY, ragPos.x, ragPos.y) < 50) {
+            draggingRag = true;
+        }
+    };
+
+    p.mouseDragged = function() {
+        if (draggingSponge) {
+            spongePos.x = p.mouseX;
+            spongePos.y = p.mouseY;
+        }
+        if (draggingRag) {
+            ragPos.x = p.mouseX;
+            ragPos.y = p.mouseY;
+        }
+    };
+
+    p.mouseReleased = function() {
+        draggingSponge = false;
+        draggingRag = false;
+
+        if (gameState === "wash") {
+            spongePos.set(440, 50);
+        }
+
+        if (gameState === "clean") {
+            ragPos.set(440, 140, 100, 100);
+        }
+    };
 }
 
-function setup() {
-    let canvas = createCanvas(canvasW, canvasH);
-    canvas.parent("game1-container");
-    imageMode(CENTER);
-    textAlign(CENTER, CENTER);
-    spongePos = createVector(440, 50);
-    ragPos = createVector(440, 140);
-}
-
-function draw() {
-    background(230, 255, 230);
-    image(pibble, width / 2, height / 2, 250, 250);
-    image(rag, ragPos.x, ragPos.y, 100, 100);
-    image(sponge, spongePos.x, spongePos.y, 100, 100);
-
-    // Main Game States
-    if (gameState == "wash") {
-        handleScrubbing("sponge");
-    } else if (gameState == "clean") {
-        handleScrubbing("rag");
-    }
-
-    // Text Feedback
-    if (gameState == "wash") {
-        push();
-        fill(70);
-        textSize(22);
-        text("wash mah belly!", width / 2, height - 25);
-    }
-    if (gameState == "yay") {
-        text("yaaayyyyyyy", width / 2, height - 25);
-    }
-    if (gameState == "clean") {
-        text("clean mah belly!", width / 2, height - 25);
-    }
-    if (gameState == "done") {
-        text("i'm very happy!", width / 2, height - 25);
-        pop();
-    }
-
-    // Progress Bar
-    if (gameState == "wash" || gameState == "clean") {
-        push();
-        fill(255);
-        rect(width / 2 - 100, 20, 200, 15, 10);
-        fill(120, 200, 120);
-        rect(width / 2 - 100, 20, map(progress, 0, 100, 0, 200), 15, 10);
-        pop();
-    }
-
-}
-
-function handleScrubbing(tool) {
-    let toolPos = (tool == "sponge") ? spongePos : ragPos;
-    let dragging = (tool == "sponge") ? draggingSponge : draggingRag;
-
-    if (dragging && dist(toolPos.x, toolPos.y, width / 2, height / 2 - 20) < 120) {
-        progress += 0.8;
-    }
-
-    if (progress >= 100 && gameState === "wash") {
-        washSound.stop();
-        progress = 0;
-        gameState = "yay";
-        yaySound.play();
-        setTimeout(() => {
-            gameState = "clean";
-            cleanSound.loop();
-        }, 2000);
-    } else if (progress >= 100 && gameState === "clean") {
-        progress= 0;
-        gameState = "done";
-        yaySound.play();
-        cleanSound.stop();
-
-        document.getElementById("nextGameBtn").style.display = "inline-block";
-    }
-
-}
-
-function mousePressed() {
-    if (dist(mouseX, mouseY, spongePos.x, spongePos.y) < 50) {
-        draggingSponge = true;
-    }
-    if (gameState === "wash" && dist(mouseX, mouseY, spongePos.x, spongePos.y) < 50) {
-        washSound.play();
-    }
-    if (dist(mouseX, mouseY, ragPos.x, ragPos.y) < 50) {
-        draggingRag = true;
-    }
-}
-
-function mouseDragged() {
-    if (draggingSponge) {
-        spongePos.x = mouseX;
-        spongePos.y = mouseY;
-    }
-    if (draggingRag) {
-        ragPos.x = mouseX;
-        ragPos.y = mouseY;
-    }
-}
-
-function mouseReleased() {
-    draggingSponge = false;
-    draggingRag = false;
-
-    if (gameState === "wash") {
-        spongePos.set(440, 50);
-    }
-
-    if (gameState === "clean") {
-        ragPos.set(440, 140, 100, 100);
-    }
-}
+new p5(pibbleSketch);
--- game2/picture.js
+++ game2/picture.js
@@ -1,275 +1,281 @@
-// Visual Assets
-const images = [
-    "joseph pictures/20220615_165224.JPG",
-    "joseph pictures/Image-1.JPG",
-    "joseph pictures/IMG_1212.png",
-    "joseph pictures/IMG_1362.png",
-    "joseph pictures/IMG_1961.jpg",
-    "joseph pictures/IMG_2465.png",
-    "joseph pictures/IMG_4478.png",
-    "joseph pictures/IMG_5124.png",
-    "joseph pictures/IMG_5783.png",
-    "joseph pictures/IMG_5835.png",
-    "joseph pictures/IMG_5996.png",
-    "joseph pictures/IMG_6422.png",
-    "joseph pictures/IMG_6426.png",
-    "joseph pictures/IMG_6752.JPG",
-    "joseph pictures/IMG_6766.png",
-    "joseph pictures/IMG_6767.png",
-    "joseph pictures/IMG_6851.png",
-    "joseph pictures/IMG_6943.png",
-    "joseph pictures/IMG_7279.png",
-    "joseph pictures/IMG_7390.png",
-    "joseph pictures/IMG_7558.png",
-    "joseph pictures/IMG_7661.png",
-    "joseph pictures/IMG_7766.png",
-    "joseph pictures/IMG_7798.png",
-    "joseph pictures/IMG_8039.png",
-    "joseph pictures/IMG_8317.png",
-    "joseph pictures/IMG_8458.png",
-    "joseph pictures/IMG_8674.png",
-    "joseph pictures/IMG_8675.png"
-];
+let pictureSketch = function(p) {
 
-// Picture Answers
-const answers = [
-    ["norco", "pumpkin rock"],
-    ["irvine", "hello kitty", "hello kitty cafe", "cafe"],
-    ["macy's", "victoria gardens", "rancho cucamonga"],
-    ["park", "providence ranch", "providence ranch park", "eastvale"],
-    ["bathroom", "mcdonalds", "mcdonald's", "mcdonalds bathroom", "mcdonald's bathroom"],
-    ["victoria gardens", "h&m", "rancho cucamonga"],
-    ["santa anita", "h&m"],
-    ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
-    ["manzanita", "dorm", "manzi", "manzi bathroom", "manzanita bathroom", "manzi bathrooms", "manzanita bathrooms", "bathroom"],
-    ["manzanita", "dorm", "manzi"],
-    ["sb", "beach", "santa barbara", "ucsb"],
-    ["sb", "santa barbara", "manzanita", "manzi", "ucsb"],
-    ["girvetz", "girvetz hall"],
-    ["chica state beach", "chica", "chica state"],
-    ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
-    ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
-    ["santa ynez", "sy"],
-    ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
-    ["goleta butterfly grove", "butterfly", "butterfly grove"],
-    ["ucsb", "north hall"],
-    ["ucsb", "campbell hall"],
-    ["santa ynez", "sy"],
-    ["buena park", "medieval times"],
-    ["buena park", "medieval times"],
-    ["student health", "shs"],
-    ["lizard's mouth", "lizards mouth"],
-    ["sb", "santa barbara", "beach", "ucsb"],
-    ["sb", "santa barbara", "beach", "ucsb"],
-    ["sb", "santa barbara", "beach", "ucsb"] 
-];
+    // Visual Assets
+    const images = [
+        "joseph pictures/20220615_165224.JPG",
+        "joseph pictures/Image-1.JPG",
+        "joseph pictures/IMG_1212.png",
+        "joseph pictures/IMG_1362.png",
+        "joseph pictures/IMG_1961.jpg",
+        "joseph pictures/IMG_2465.png",
+        "joseph pictures/IMG_4478.png",
+        "joseph pictures/IMG_5124.png",
+        "joseph pictures/IMG_5783.png",
+        "joseph pictures/IMG_5835.png",
+        "joseph pictures/IMG_5996.png",
+        "joseph pictures/IMG_6422.png",
+        "joseph pictures/IMG_6426.png",
+        "joseph pictures/IMG_6752.JPG",
+        "joseph pictures/IMG_6766.png",
+        "joseph pictures/IMG_6767.png",
+        "joseph pictures/IMG_6851.png",
+        "joseph pictures/IMG_6943.png",
+        "joseph pictures/IMG_7279.png",
+        "joseph pictures/IMG_7390.png",
+        "joseph pictures/IMG_7558.png",
+        "joseph pictures/IMG_7661.png",
+        "joseph pictures/IMG_7766.png",
+        "joseph pictures/IMG_7798.png",
+        "joseph pictures/IMG_8039.png",
+        "joseph pictures/IMG_8317.png",
+        "joseph pictures/IMG_8458.png",
+        "joseph pictures/IMG_8674.png",
+        "joseph pictures/IMG_8675.png"
+    ];
 
-// Music Playlist
-let playlist = [];
-let currentTrackIndex = 0;
-let playing = false;
+    // Picture Answers
+    const answers = [
+        ["norco", "pumpkin rock"],
+        ["irvine", "hello kitty", "hello kitty cafe", "cafe"],
+        ["macy's", "victoria gardens", "rancho cucamonga"],
+        ["park", "providence ranch", "providence ranch park", "eastvale"],
+        ["bathroom", "mcdonalds", "mcdonald's", "mcdonalds bathroom", "mcdonald's bathroom"],
+        ["victoria gardens", "h&m", "rancho cucamonga"],
+        ["santa anita", "h&m"],
+        ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
+        ["manzanita", "dorm", "manzi", "manzi bathroom", "manzanita bathroom", "manzi bathrooms", "manzanita bathrooms", "bathroom"],
+        ["manzanita", "dorm", "manzi"],
+        ["sb", "beach", "santa barbara", "ucsb"],
+        ["sb", "santa barbara", "manzanita", "manzi", "ucsb"],
+        ["girvetz", "girvetz hall"],
+        ["chica state beach", "chica", "chica state"],
+        ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
+        ["sb", "santa barbara", "sb botanical garden", "santa barbara botanical garden", "botanical gardens", "garden", "gardens", "sb botanical gardens", "santa barbara botanical gardens"],
+        ["santa ynez", "sy"],
+        ["iv", "isla vista", "pumpkin patch", "isla vista pumpkin patch", "iv pumpkin patch"],
+        ["goleta butterfly grove", "butterfly", "butterfly grove"],
+        ["ucsb", "north hall"],
+        ["ucsb", "campbell hall"],
+        ["santa ynez", "sy"],
+        ["buena park", "medieval times"],
+        ["buena park", "medieval times"],
+        ["student health", "shs"],
+        ["lizard's mouth", "lizards mouth"],
+        ["sb", "santa barbara", "beach", "ucsb"],
+        ["sb", "santa barbara", "beach", "ucsb"],
+        ["sb", "santa barbara", "beach", "ucsb"] 
+    ];
 
-// Sound Assets
-let el_dorado;
-let pink_skies;
-let ticking;
-let tourniquet;
+    // Music Playlist
+    let playlist = [];
+    let currentTrackIndex = 0;
+    let playing = false;
 
-// Game Variables
-const g2_total = images.length;
-let img;
-let idx = 0;
-let reveal = false;
-let canvasW = 640;
-let canvasH = 400;
-let flashRadius = 90;
+    // Sound Assets
+    let el_dorado;
+    let pink_skies;
+    let ticking;
+    let tourniquet;
 
-function preload() {
-    el_dorado = loadSound("music/Zach Bryan - El Dorado.mp3");
-    pink_skies = loadSound("music/Zach Bryan - Pink Skies.mp3");
-    ticking = loadSound("music/Zach Bryan - Ticking.mp3");
-    tourniquet = loadSound("music/Zach Bryan - Tourniquet.mp3");
+    // Game Variables
+    const g2_total = images.length;
+    let img;
+    let idx = 0;
+    let reveal = false;
+    let canvasW = 640;
+    let canvasH = 400;
+    let flashRadius = 90;
 
-    playlist = [el_dorado, pink_skies, ticking, tourniquet];
+    p.preload = function() {
+        el_dorado = p.loadSound("music/Zach Bryan - El Dorado.mp3");
+        pink_skies = p.loadSound("music/Zach Bryan - Pink Skies.mp3");
+        ticking = p.loadSound("music/Zach Bryan - Ticking.mp3");
+        tourniquet = p.loadSound("music/Zach Bryan - Tourniquet.mp3");
 
-    loadG2Level(idx);
-}
+        playlist = [el_dorado, pink_skies, ticking, tourniquet];
 
-function setup() {
-    let cnv = createCanvas(canvasW, canvasH);
-    cnv.parent("game2-canvas");
-    imageMode(CENTER);
-    textAlign(CENTER, CENTER);
-    textSize(16);
+        loadG2Level(idx);
+    };
 
-    const form = document.getElementById("g2-form");
-    const input = document.getElementById("g2-input");
-    const nextBtn = document.getElementById("g2-next");
+    p.setup = function() {
+        let cnv = p.createCanvas(canvasW, canvasH);
+        cnv.parent("game2-canvas");
+        p.imageMode(p.CENTER);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textSize(16);
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        submitG2Guess(input.value);
-    });
+        const form = document.getElementById("g2-form");
+        const input = document.getElementById("g2-input");
+        const nextBtn = document.getElementById("g2-next");
 
-    nextBtn.addEventListener("click", () => {
-        if (idx === g2_total - 1) {
-            switchSection("game2", "login");
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            submitG2Guess(input.value);
+        });
+
+        nextBtn.addEventListener("click", () => {
+            if (idx === g2_total - 1) {
+                switchSection("game2", "login");
+                return;
+            }
+            goToNextG2Level();
+        });
+
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "Enter" && reveal === true) {
+                e.preventDefault();
+                goToNextG2Level();
+            }
+        });
+
+        loadG2Level(idx);
+        updateG2LevelLabel();
+    };
+
+    p.draw = function() {
+        p.background(0, 255);
+
+        if (!img) {
+            p.fill(80);
+            p.text("loading image...", p.width/2, p.height/2);
             return;
         }
-        goToNextG2Level();
-    });
 
-    document.addEventListener("keydown", (e) => {
-        if (e.key === "Enter" && reveal === true) {
-            e.preventDefault();
-            goToNextG2Level();
+        drawImageContain(img, p.width, p.height);
+
+        if (!reveal) {
+            p.noStroke();
+
+            let overlay = p.createGraphics(p.width, p.height);
+            overlay.clear();
+            overlay.fill(0, 255);
+            overlay.rect(0, 0, p.width, p.height);
+
+            overlay.erase();
+            overlay.circle(p.constrain(p.mouseX, 0, p.width), p.constrain(p.mouseY, 0, p.height), flashRadius * 2);
+            overlay.noErase();
+
+            p.image(overlay, p.width/2, p.height/2, p.width, p.height);
+
+        } else {
+            drawImageContain(img, p.width, p.height);
         }
-    });
 
-    loadG2Level(idx);
-    updateG2LevelLabel();
-}
+    };
 
-function draw() {
-    background(0, 255);
+    function loadG2Level(idx) {
+        reveal = false;
+        const imgPath = images[idx];
+        img = null;
 
-    if (!img) {
-        fill(80);
-        text("loading image...", width/2, height/2);
-        return;
-    }
+        p.loadImage(imgPath, (gimg) => {
+            img = gimg;
+        });
 
-    drawImageContain(img, width, height);
-
-    if (!reveal) {
-        noStroke();
-
-        let overlay = createGraphics(width, height);
-        overlay.clear();
-        overlay.fill(0, 255);
-        overlay.rect(0, 0, width, height);
-
-        overlay.erase();
-        overlay.circle(constrain(mouseX, 0, width), constrain(mouseY, 0, height), flashRadius * 2);
-        overlay.noErase();
-
-        image(overlay, width/2, height/2, width, height);
-
-    } else {
-        drawImageContain(img, width, height);
-    }
-
-}
-
-function loadG2Level(idx) {
-    reveal = false;
-    const imgPath = images[idx];
-    img = null;
-
-    loadImage(imgPath, (gimg) => {
-        img = gimg;
-    });
-
-    const feedback = document.getElementById("g2-feedback");
-    const success = document.getElementById("g2-success");
-    const nextBtn = document.getElementById("g2-next");
-    const input = document.getElementById("g2-input");
-
-    feedback.style.visiblity = "hidden";
-    success.style.display = "none";
-    nextBtn.style.display = "none";
-    input.value = "";
-    input.focus();
-}
-
-function updateG2LevelLabel() {
-    const label = document.getElementById("g2-level");
-    label.textContent = `Level ${idx + 1} of ${g2_total}`;
-}
-
-function normalizeAnswer(s) {
-    return s
-        .toLowerCase()
-        .trim()
-        .replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, "")
-        .replace(/\s+/g, " ");
-}
-
-function submitG2Guess(raw) {
-    const feedback = document.getElementById("g2-feedback");
-    const success = document.getElementById("g2-success");
-    const nextBtn = document.getElementById("g2-next");
-
-    const guess = normalizeAnswer(raw);
-    if (!guess) {
-        feedback.textContent = "Please enter a guess.";
-        feedback.style.visibility = "visible";
-        return;
-    }
-
-    const answerss = (answers[idx] || []).map(normalizeAnswer);
-
-    const correct = answerss.some(a => a === guess);
-
-    if (correct) {
-        reveal = true;
-        feedback.style.visibility = "hidden";
-        success.style.display = "block";
-        nextBtn.style.display = "inline-block";
-
-        if (!playing) startGame2Music();
-    } else {
-        feedback.textContent = "Incorrect, try again.";
-        feedback.style.visibility = "visible";
-    }
-} 
-
-function goToNextG2Level() {
-    if (idx < g2_total - 1) {
-        idx += 1;
-        updateG2LevelLabel();
-        loadG2Level(idx);
-    } else {
+        const feedback = document.getElementById("g2-feedback");
         const success = document.getElementById("g2-success");
         const nextBtn = document.getElementById("g2-next");
-        success.textContent = "All levels complete!";
-        nextBtn.textContent = "Continue";
-        nextBtn.style.display = "inline-block";
-        nextBtn.style.visibility = "none";
+        const input = document.getElementById("g2-input");
+
+        feedback.style.visiblity = "hidden";
+        success.style.display = "none";
+        nextBtn.style.display = "none";
+        input.value = "";
+        input.focus();
     }
-}
 
-function drawImageContain(gimg, boxW, boxH) {
-    const iw = gimg.width;
-    const ih = gimg.height;
-    const scale = Math.min(boxW / iw, boxH / ih);
-    const w = iw * scale;
-    const h = ih * scale;
-    image(gimg, boxW/2, boxH/2, w, h);
-}
+    function updateG2LevelLabel() {
+        const label = document.getElementById("g2-level");
+        label.textContent = `Level ${idx + 1} of ${g2_total}`;
+    }
 
-function startGame2Music() {
-    if (playing) return;
-    playing = true;
+    function normalizeAnswer(s) {
+        return s
+            .toLowerCase()
+            .trim()
+            .replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, "")
+            .replace(/\s+/g, " ");
+    }
 
-    currentTrackIndex = 0;
+    function submitG2Guess(raw) {
+        const feedback = document.getElementById("g2-feedback");
+        const success = document.getElementById("g2-success");
+        const nextBtn = document.getElementById("g2-next");
 
-    playNextTrack();
-}
+        const guess = normalizeAnswer(raw);
+        if (!guess) {
+            feedback.textContent = "Please enter a guess.";
+            feedback.style.visibility = "visible";
+            return;
+        }
 
-function playNextTrack() {
-    playlist.forEach(track => track.stop());
+        const answerss = (answers[idx] || []).map(normalizeAnswer);
 
-    let track = playlist[currentTrackIndex];
+        const correct = answerss.some(a => a === guess);
 
-    if (currentTrackIndex === playlist.length - 1) {
+        if (correct) {
+            reveal = true;
+            feedback.style.visibility = "hidden";
+            success.style.display = "block";
+            nextBtn.style.display = "inline-block";
+
+            if (!playing) startGame2Music();
+        } else {
+            feedback.textContent = "Incorrect, try again.";
+            feedback.style.visibility = "visible";
+        }
+    } 
+
+    function goToNextG2Level() {
+        if (idx < g2_total - 1) {
+            idx += 1;
+            updateG2LevelLabel();
+            loadG2Level(idx);
+        } else {
+            const success = document.getElementById("g2-success");
+            const nextBtn = document.getElementById("g2-next");
+            success.textContent = "All levels complete!";
+            nextBtn.textContent = "Continue";
+            nextBtn.style.display = "inline-block";
+            nextBtn.style.visibility = "none";
+        }
+    }
+
+    function drawImageContain(gimg, boxW, boxH) {
+        const iw = gimg.width;
+        const ih = gimg.height;
+        const scale = Math.min(boxW / iw, boxH / ih);
+        const w = iw * scale;
+        const h = ih * scale;
+        p.image(gimg, boxW/2, boxH/2, w, h);
+    }
+
+    function startGame2Music() {
+        if (playing) return;
+        playing = true;
+
         currentTrackIndex = 0;
-    } else {
-        track.play();
 
-        track.onended(() => {
-            currentTrackIndex++;
-            playNextTrack();
-        });
+        playNextTrack();
     }
-}
+
+    function playNextTrack() {
+        playlist.forEach(track => track.stop());
+
+        let track = playlist[currentTrackIndex];
+
+        if (currentTrackIndex === playlist.length - 1) {
+            currentTrackIndex = 0;
+        } else {
+            track.play();
+
+            track.onended(() => {
+                currentTrackIndex++;
+                playNextTrack();
+            });
+        }
+    }
+
+};
+
+new p5(pictureSketch);
